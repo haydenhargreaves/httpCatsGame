10e329d7d41c516377e18e08849f69f01d0e27f8
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-04-10 23:45:06 +0000

    ANSIBLE: Working on ansible implementation

--- shell.nix
+++ shell.nix
@@ -2,11 +2,19 @@
 
 pkgs.mkShell {
 
-  buildInputs = [];
-  
+  buildInputs = [
+    pkgs.ansible
+    pkgs.python312
+    pkgs.python312Packages.python-dotenv
+    pkgs.python312Packages.ansible
+    pkgs.python312Packages.streamlit
+    pkgs.python312Packages.digital-ocean
+  ];
   # Pretty much all we need for this project
   shellHook = ''
     # fixes libstdc++ issues and libgl.so issues
     LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/
+
+    zsh
   '';
 }
